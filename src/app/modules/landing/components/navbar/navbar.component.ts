6f36ffd4fa1d4ca6105542f9e58bbf1da7cb76b2
amani1505 <amanideveloper1505@gmail.com>
2024-10-08 15:13:21 +0300

    Finished nav bar

--- src/app/modules/landing/components/navbar/navbar.component.ts
+++ src/app/modules/landing/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, Inject, Input } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule,AngularSvgIconModule],
+  imports: [CommonModule, RouterModule, AngularSvgIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
@@ -14,6 +14,8 @@ export class NavbarComponent {
   @Input() isScrolled: boolean = false;
   showMenu: boolean = false;
   isAnimating = false;
+
+  constructor(private _router: Router) {}
 
   openMobileNav() {
     this.showMenu = true;
@@ -27,9 +29,23 @@ export class NavbarComponent {
 
   handleAnimationEnd() {
     if (!this.showMenu) {
-      this.isAnimating = false; 
-      
+      this.isAnimating = false;
+
       document.body.classList.remove('overflow-hidden');
     }
+  }
+  navigate(route: string) {
+    this._router.navigate([route]);
+    this.showMenu = false;
+    this.isAnimating = true;
+  }
+  isActiveRoute(route: string): boolean {
+    return this._router.url === route;
+  }
+
+  // Method for partial match of routes
+  isPartialActiveRoute(route: string): boolean {
+    return this._router.isActive(route, false); 
+
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    colors: {
-      primary: "hsl(12, 98%, 52%)",
-      medium: "rgba(0,0,0,0.5)",
-      secondary: "#c0c0c0",
-      black: "black",
-      white: "white",
-    },
-    fontFamily: {
-      "pt-serif": ["PT Serif", "serif"],
-      montserrat: ["Montserrat", "sans-serif"],
-      raleway: ["Raleway"],
-      varela: ["Varela Round", "sans-serif"],
-    },
     extend: {
+      colors: {
+        primary: "hsl(12, 98%, 52%)",
+        medium: "rgba(0,0,0,0.5)",
+        secondary: "#2d6f17",
+        black: "black",
+        white: "white",
+      },
+      fontFamily: {
+        "pt-serif": ["PT Serif", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        raleway: ["Raleway"],
+        varela: ["Varela Round", "sans-serif"],
+      },
       keyframes: {
-        'slide-up': {
-          '0%': { transform: 'translateY(100%)' },
-          '100%': { transform: 'translateY(0)' },
+        "slide-up": {
+          "0%": { transform: "translateY(100%)" },
+          "100%": { transform: "translateY(0)" },
         },
-        'slide-down': {
-          '0%': { transform: 'translateY(0)' },
-          '100%': { transform: 'translateY(100%)' },
+        "slide-down": {
+          "0%": { transform: "translateY(0)" },
+          "100%": { transform: "translateY(100%)" },
         },
       },
       animation: {
-        'slide-up': 'slide-up 0.5s ease-out',
-        'slide-down': 'slide-down 0.5s ease-out',
+        "slide-up": "slide-up 0.5s ease-out",
+        "slide-down": "slide-down 0.5s ease-out",
       },
     },
   },
   plugins: [],
-}
+};
