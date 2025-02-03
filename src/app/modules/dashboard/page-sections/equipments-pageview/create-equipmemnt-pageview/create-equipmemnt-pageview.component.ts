cdc891f58a18b58c50c9b33b6f51818f105e6c4f
amani1505 <amanideveloper1505@gmail.com>
2025-02-03 12:33:35 +0300

    Adding category

--- src/app/modules/dashboard/page-sections/equipments-pageview/create-equipmemnt-pageview/create-equipmemnt-pageview.component.ts
+++ src/app/modules/dashboard/page-sections/equipments-pageview/create-equipmemnt-pageview/create-equipmemnt-pageview.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ImageUploaderComponent } from '@components/image-uploader/image-uploader.component';
+import { NormalSelectComponent } from '@components/select/normal-select/normal-select.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'create-equipmemnt-pageview',
   standalone: true,
-  imports: [],
+  imports: [
+    AngularSvgIconModule,
+    NormalSelectComponent,
+    ImageUploaderComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    NgClass
+  ],
   templateUrl: './create-equipmemnt-pageview.component.html',
-  styleUrl: './create-equipmemnt-pageview.component.scss'
+  styleUrl: './create-equipmemnt-pageview.component.scss',
 })
 export class CreateEquipmemntPageviewComponent {
+  private readonly _formBuilder = inject(FormBuilder);
 
+  options = [
+    {
+      label: 'New Arrival',
+      value: 'new arrival',
+    },
+    {
+      label: 'Out of Stock',
+      value: 'out of stock',
+    },
+    {
+      label: 'Restocked',
+      value: 'restocked',
+    },
+    {
+      label: 'Upcomming Stock',
+      value: 'upcomming stock',
+    },
+  ];
+
+  equipmentForm = this._formBuilder.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    isPublished: ['', Validators.required],
+    model: ['', Validators.required],
+    serial_number: ['', Validators.required],
+    purchase_date: ['', Validators.required],
+    price: [1, Validators.required],
+    quantity: [1, Validators.required],
+    status: ['', Validators.required],
+    used_for: ['', Validators.required],
+  });
 }
